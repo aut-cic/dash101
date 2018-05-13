226ad08d81a58a08e8d6fc176a1ec140bd676a86
Parham Alvani <parham.alvani@gmail.com>
2018-05-13 22:40:22 +0430

    Adds main go server.

---
+++ main.go
@@ -0,0 +1,61 @@
+/*
+ * +===============================================
+ * | Author:        Parham Alvani <parham.alvani@gmail.com>
+ * |
+ * | Creation Date: 06-05-2018
+ * |
+ * | File Name:     hello.go
+ * +===============================================
+ */
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func handle() http.Handler {
+	r := gin.Default()
+
+	r.Static("/", "./")
+
+	return r
+}
+
+func main() {
+	r := handle()
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+
+	go func() {
+		fmt.Printf("Registerator Listen: %s\n", srv.Addr)
+		// service connections
+		if err := srv.ListenAndServe(); err != nil {
+			log.Fatal("Listen Error:", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	fmt.Println("Registerator Shutdown")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Shutdown Error:", err)
+	}
+}
