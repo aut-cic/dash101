2fb582838f6f184c8238695cc24234e549429400
Parham Alvani <parham.alvani@gmail.com>
2018-07-11 19:31:07 +0430

    Removes segment ranges and add comments.

--- actions/home.go
+++ actions/home.go
@@ -16,7 +16,7 @@ func HomeHandler(c buffalo.Context) error {
 // MPDHandler returns Media Presentation Description
 func MPDHandler(c buffalo.Context) error {
 	// https://en.wikipedia.org/wiki/ISO_8601#Durations
-	m := mpd.NewMPD(mpd.DASH_PROFILE_ONDEMAND, "PT30S", "PT1.97S")
+	m := mpd.NewMPD(mpd.DASH_PROFILE_ONDEMAND, "PT5S", "PT1.97S")
 
 	// Audio
 	audioAS, err := m.AddNewAdaptationSetAudio(mpd.DASH_MIME_TYPE_AUDIO_MP4, true, 1, "und")
@@ -37,19 +37,23 @@ func MPDHandler(c buffalo.Context) error {
 		return c.Error(http.StatusInternalServerError, err)
 	}
 
-	videoRep1, err := videoAS.AddNewRepresentationVideo(1100690, "avc1.4d401e", "800k/video-1", "30000/1001", 640, 360)
+	// bandwidth - in Bits/s (i.e. 1518664).
+	// codecs - codec string for Audio Only (in RFC6381, https://tools.ietf.org/html/rfc6381) (i.e. avc1.4d401f).
+	// id - ID for this representation, will get used as $RepresentationID$ in template strings.
+	// frameRate - video frame rate (as a fraction) (i.e. 30000/1001).
+	// width - width of the video (i.e. 1280).
+	// height - height of the video (i.e 720).
+	videoRep1, err := videoAS.AddNewRepresentationVideo(1100690, "avc1.4d401e", "800k/video-1", "30000/1001", 640, 480)
 	if err != nil {
 		return c.Error(http.StatusInternalServerError, err)
 	}
 	videoRep1.SetNewBaseURL("/cdn/800k/output-video-1.mp4")
-	videoRep1.AddNewSegmentBase("686-813", "0-685")
 
 	videoRep2, err := videoAS.AddNewRepresentationVideo(1633516, "avc1.4d401f", "1200k/video-1", "30000/1001", 960, 540)
 	if err != nil {
 		return c.Error(http.StatusInternalServerError, err)
 	}
 	videoRep2.SetNewBaseURL("/cdn/1200k/output-video-1.mp4")
-	videoRep2.AddNewSegmentBase("686-813", "0-685")
 
 	// Subtitle
 	subtitleAS, err := m.AddNewAdaptationSetSubtitle(mpd.DASH_MIME_TYPE_SUBTITLE_VTT, "en")
